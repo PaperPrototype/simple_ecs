484d433c40db64f192a01566df7e94d78691758d
Abdiel Lopez <a.paperprototype@gmail.com>
2021-08-16 15:22:10 -0400

    updated stuff

--- attempt_4/ecs.c
+++ attempt_4/ecs.c
@@ -40,11 +40,10 @@ void* get_aspect(aspect_t* archetype, char* type)
 
 int main(void)
 {
-    int num_aspects_in_archetype = 2;
     int num_components = 2;
 
     // archetype is a combination of aspects (component types)
-    aspect_t* archetype = malloc(sizeof(aspect_t) * num_aspects_in_archetype);
+    aspect_t* archetype = malloc(sizeof(aspect_t) * 2);
 
     archetype[0].components = new_generic_array(sizeof(pos_t), num_components);
     archetype[0].type = "pos";
@@ -55,15 +54,24 @@ int main(void)
     archetype[1].components = new_generic_array(sizeof(int), num_components);
     archetype[1].type = "int";
 
-    set_item(0, sizeof(int), archetype[1].components, (int)12);
-    set_item(1, sizeof(int), archetype[1].components, (int)13);
+    int tmp1 = 12;
+    int tmp2 = 13;
+    set_item(0, sizeof(int), archetype[1].components, &tmp1);
+    set_item(1, sizeof(int), archetype[1].components, &tmp2);
 
     for (int i = 0; i < num_components; i++)
     {
+        printf("entity %i ", i);
+
         //                                      aspect 0 = pos
         pos_t* pos = get_item(i, sizeof(pos_t), archetype[0].components);
+        printf("pos (%f, %f, %f) ", pos->x, pos->y, pos->z);
+
         //                                        aspect 1 = int 
-        pos_t* integer = get_item(i, sizeof(int), archetype[1].components);
+        int* integer = get_item(i, sizeof(int), archetype[1].components);
+        printf("int %i ", *integer);
+
+        printf("\n");
     }
 
     // free memory
